45be032701b7b5afb8beb9f70dc08041beceb67c
Akshay369vm <85275123+Akshay369vm@users.noreply.github.com>
2022-07-28 16:06:40 +0530

    Create sharedlibtest.groovy

---
+++ vars/sharedlibtest.groovy
@@ -0,0 +1,12 @@
+def call(String repoUrl){
+  pipeline {
+       agent any
+       stages{
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${repoUrl}"
+            }   } 
+        }
+    }
+}     
